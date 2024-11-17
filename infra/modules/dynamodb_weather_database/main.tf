7318989c11c8427f390e69c060eaca28eb0348a1
Valderas7 <valderas777@gmail.com>
2024-11-18 00:48:02 +0100

    fix: corregido recurso tabla Dynamo

--- infra/modules/dynamodb_weather_database/main.tf
+++ infra/modules/dynamodb_weather_database/main.tf
@@ -8,50 +8,6 @@ resource "aws_dynamodb_table" "weather_db" {
     name = "Id"
     type = "N"
   }
-  attribute {
-    name = "Nombre"
-    type = "S"
-  }
-  attribute {
-    name = "Situación_meteorológica"
-    type = "S"
-  }
-  attribute {
-    name = "Temperatura"
-    type = "N"
-  }
-  attribute {
-    name = "Presión"
-    type = "N"
-  }
-  attribute {
-    name = "Humedad"
-    type = "N"
-  }
-  attribute {
-    name = "Velocidad_viento"
-    type = "N"
-  }
-  attribute {
-    name = "Nubosidad"
-    type = "N"
-  }
-  attribute {
-    name = "Precipitaciones"
-    type = "N"
-  }
-  attribute {
-    name = "Nevada"
-    type = "N"
-  }
-  attribute {
-    name = "Latitud"
-    type = "N"
-  }
-  attribute {
-    name = "Longitud"
-    type = "N"
-  }
   tags = {
     Project = "TFM"
   }
