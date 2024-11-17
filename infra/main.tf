91cbfc4c091c1f8d9d1881b48a385dd933f51ee8
Valderas7 <valderas777@gmail.com>
2024-11-17 23:33:59 +0100

    feat: enlazado módulo DynamoDB

--- infra/main.tf
+++ infra/main.tf
@@ -4,3 +4,9 @@ module "lambda_weather_database" {
   source  = "./modules/lambda_weather_database"
   api_key = var.openweather_api_key
 }
+
+# Se llama al módulo '' pasando el valor de la API KEY
+# (que está en 'var.openweather_api_key') a la variable 'api_key' del módulo
+module "dynamo_weather_database" {
+  source  = "./modules/dynamodb_weather_database"
+}
