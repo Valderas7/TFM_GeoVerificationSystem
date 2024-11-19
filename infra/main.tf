be96e56e3c177fcb06063572e2fe8aea9de467a0
Valderas7 <valderas777@gmail.com>
2024-11-19 01:44:09 +0100

    feat: módulo para backend remoto

--- infra/main.tf
+++ infra/main.tf
@@ -1,39 +1,23 @@
-# Se establece que el 'backend' (el '.tfstate') se va a almacenar en remoto en
-# un bucket de S3
-terraform {
-  backend "s3" {
-    bucket         = "tfm_bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
-  }
-}
-
-
-# Recurso para crear una tabla de DynamoDB para habilitar el bloqueo de estado
-# ('state locking') del backend para evitar que múltiples ejecuciones de
-# Terraform modifiquen el estado simultáneamente
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+# Se llama al módulo 'backend' para crear los recursos (bucket y tabla
+# de DynamoDB) para almacenar el backend (archivo 'tfstate') en remoto
+module "lambda_weather_database" {
+  source  = "./modules/backend"
 }
 
 
 # Se llama al módulo 'lambda_weather_database' pasando el valor de la API KEY
 # (que está en 'var.openweather_api_key') a la variable 'api_key' del módulo
+# para crear la función Lambda que realiza consultas a la API de
+# OpenWeatherMap y guarda los registros en una tabla de DynamoDB
 module "lambda_weather_database" {
   source  = "./modules/lambda_weather_database"
   api_key = var.openweather_api_key
 }
 
 
-# Se llama al módulo 'dynamo_weather_database'
+# Se llama al módulo 'dynamo_weather_database' para crear la base de datos de
+# DynamoDB en la que almacenar los datos de la API de OpenWeatherMap
+# consultados con la función Lambda
 module "dynamo_weather_database" {
   source  = "./modules/dynamodb_weather_database"
 }
