1942a43f42881211b65d21958ce1c1d4a436df80
Valderas7 <valderas777@gmail.com>
2024-11-20 00:16:53 +0100

    fix: layer creada de nuevo con todas librerías en misma carpeta

--- infra/modules/lambda_weather_database/main.tf
+++ infra/modules/lambda_weather_database/main.tf
@@ -3,6 +3,7 @@ resource "aws_lambda_layer_version" "tfm_layer" {
   layer_name = var.layer_name
   filename   = var.layer_path
   source_code_hash = filebase64sha256(var.layer_path)
+  compatible_runtimes = ["python3.11"]
 }
 
 
