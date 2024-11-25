823f2462992e824db1e4d1097d974d11c98b7aa5
Valderas7 <valderas777@gmail.com>
2024-11-25 01:23:16 +0100

    docs: 'outputs.tf' del módulo de 'lambda_query_weather_data'

--- infra/modules/lambda_query_weather_data/outputs.tf
+++ infra/modules/lambda_query_weather_data/outputs.tf
@@ -1,5 +1,5 @@
 # Bloque de salida para recopilar el URI de la Lambda creada
 output "lambda_uri" {
-  description = "ARN de la Lambda utilizada para consultar datos climáticos"
+  description = "URI de la Lambda utilizada para consultar datos climáticos"
   value       = aws_lambda_function.dynamodb_weather_query.invoke_arn
 }
