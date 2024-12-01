8d73816b9136fd963b0b12f896dcf58afdcc26a8
Valderas7 <valderas777@gmail.com>
2024-12-02 00:16:47 +0100

    feat: WIP - mapa unicode con todas las provincias

--- src/web_application/main.py
+++ src/web_application/main.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import requests
 import leafmap.foliumap as leafmap
+import folium
+from unidecode import unidecode
 
 # Se guarda en una variable la URL de la API Gateway desplegada con AWS
 api_gateway_url = 'https://mjb5qk45si.execute-api.us-east-1.amazonaws.com/prod'
@@ -17,7 +19,7 @@ action = st.sidebar.radio("Elige una opción:",
 if action == "Ver mapa general":
 
     # Título de la Web App con cabecera H2
-    st.markdown("<h2 style='text-align: center; color: black;'> Clima actual en España </h2>",
+    st.markdown("<h2 style='text-align: center; color: black;'> Clima Actual en España </h2>",
                 unsafe_allow_html=True)
     
     # Descripción de lo que realiza la aplicación web
@@ -32,13 +34,85 @@ if action == "Ver mapa general":
     # Se crea un mapa centrado en Madrid
     m = leafmap.Map(center=(40.4168, -3.7038), zoom=6)
 
-    # Agregar marcadores básicos con coordenadas y descripciones
-    m.add_marker(location=(40.4168, -3.7038), popup="Madrid: Capital de España")
-    m.add_marker(location=(41.3879, 2.16992), popup="Barcelona: Ciudad cosmopolita")
-    m.add_marker(location=(37.3891, -5.9845), popup="Sevilla: Famosa por su cultura y arte")
+    # Se realiza petición GET a la API Gateway en /clima
+    response = requests.get(f"{api_gateway_url}/clima").json()
 
-    # Mostrar el mapa en la app de Streamlit
-    m.to_streamlit(height=600)
+    # Se veritica si la clave 'data' está en la respuesta JSON...
+    if 'data' in response:
+
+        # En ese caso se almacena la clave 'data', que es la lista de todos
+        # los JSON de respuesta de todas las provincias y ciudades autónomas
+        response_list = response['data']
+    
+    # Si no...
+    else:
+        
+        # Mensaje de error de 'streamlit'
+        st.error("No se pudo obtener datos de la API. Revisa la conexión.")
+        st.stop()
+
+    # Para cada diccionario de la lista...
+    for provincia_dict in response_list:
+        
+        # Se extraen los campos de 'Latitud' y 'Longitud'
+        lat = provincia_dict.get("Latitud")
+        lon = provincia_dict.get("Longitud")
+
+        # Si la latitud es un 'string'...
+        if isinstance(lat, str):
+
+            # Intenta ejecutar el bloque de código
+            try:
+
+                # Se convierte el valor a un float
+                lat = float(lat)
+            
+            # Si hay error...
+            except ValueError:
+
+                # Se muestra mensaje de 'warning' y se continúa
+                st.warning(f"Latitud inválida para {provincia_dict['Nombre']}: {lat}")
+                continue
+
+        # Si la longitud es un 'string'...
+        if isinstance(lon, str):
+
+            # Intenta ejecutar el bloque de código
+            try:
+
+                # Se convierte el valor a un float
+                lon = float(lon)
+
+            # Si hay error...
+            except ValueError:
+
+                # Se muestra mensaje de 'warning' y se continúa
+                st.warning(f"Longitud inválida para {provincia_dict['Nombre']}: {lon}")
+                continue
+        
+        # Si la latitud o la longitud son None...
+        if lat is None or lon is None:
+
+            # Se muestra mensaje de 'warning' y se continúa
+            st.warning(f"Coordenadas faltantes para {provincia_dict['Nombre']}")
+            continue
+
+        # Se crea un contenido 'popup'
+        popup_content = (
+            f"Provincia: {unidecode(provincia_dict['Nombre'])}<br>"
+            f"Clima: {provincia_dict['Clima']}<br>"
+            f"Temperatura: {provincia_dict['Temperatura']} &#176;C<br>"
+            f"Humedad: {provincia_dict['Humedad']}%"
+        )
+        
+        # Se crea el popup con el contenido y se especifica su ancho máximo
+        popup = folium.Popup(popup_content, max_width=400)
+
+        # Añadir el marcador con el icono y el popup al mapa
+        m.add_marker(location=([lat, lon]), popup=popup)
+    
+    # Renderiza el mapa en Streamlit
+    m.to_streamlit()
 
 # Si se elige la opción de consultar los datos de una provincia específica...
 elif action == "Consultar provincia específica":
