102df5058736fdeef45915aaf36260caf7c551f1
Valderas7 <valderas777@gmail.com>
2024-12-10 01:34:28 +0100

    fix: cluster y definición de tarea de ECS

--- infra/modules/ecr_weather_repo/main.tf
+++ infra/modules/ecr_weather_repo/main.tf
@@ -1,7 +1,6 @@
 # Recurso para crear un repositorio dentro de ECR
 resource "aws_ecr_repository" "weather_repo" {
   name         = var.name
-  force_delete = true
 
   tags = {
     "Project" = "TFM"
