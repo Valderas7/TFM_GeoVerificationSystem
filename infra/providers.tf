fe84ca76e979ee620127c20c222af99df1d5da79
Valderas7 <valderas777@gmail.com>
2024-12-10 21:22:45 +0100

    feat: versiones para plugins de AWS y Terraform requeridas

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,8 +3,10 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 5.74.0"
     }
   }
+  required_version = ">= 1.5.0"
 }
 
 # Configuración para el provider de AWS
