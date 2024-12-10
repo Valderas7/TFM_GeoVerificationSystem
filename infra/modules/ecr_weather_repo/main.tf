2c509782b9e9c556db4ab486be49d859cbf48fe3
Valderas7 <valderas777@gmail.com>
2024-12-10 01:55:00 +0100

    feat: política de ciclo de vida de imágenes para ECR

--- infra/modules/ecr_weather_repo/main.tf
+++ infra/modules/ecr_weather_repo/main.tf
@@ -1,8 +1,32 @@
 # Recurso para crear un repositorio dentro de ECR
 resource "aws_ecr_repository" "weather_repo" {
-  name         = var.name
+  name = var.name
 
   tags = {
     "Project" = "TFM"
   }
+}
+
+
+# Recurso para crear una política de ciclo de vida para el repositorio, de
+# forma que se eliminan imágenes antiguas si se sube una nueva al
+# repositorio con un 'tag' ya usado anteriormente
+resource "aws_ecr_lifecycle_policy" "streamlit_lifecycle_policy" {
+  repository = aws_ecr_repository.weather_repo.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1 # Prioridad de la regla
+        description  = "Eliminar Imágenes Antiguas con el mismo Tag"
+        action = {
+          type = "expire" # Acción cuando se cumpla la regla (eliminar imágenes)
+        }
+        filter = {
+          tagStatus     = "tagged" # Aplica a imágenes etiquetadas
+          tagPrefixList = []       # Aplica a imágenes con cualquier 'tag'
+        }
+        imageCountMoreThan = 1 # Mantiene una imagen con el mismo tag, eliminando las anteriores
+      }
+    ]
+  })
 }
