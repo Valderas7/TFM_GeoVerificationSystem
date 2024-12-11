dc230533e09339520420c4096d987e1102aedaeb
Valderas7 <valderas777@gmail.com>
2024-12-11 01:25:12 +0100

    fix: memoria y CPU definido a nivel de tarea en ECS

--- infra/modules/ecs_weather_cluster/main.tf
+++ infra/modules/ecs_weather_cluster/main.tf
@@ -10,13 +10,15 @@ resource "aws_ecs_cluster" "web_application_cluster" {
 
 # Recurso para crear una definición de tarea y así especificar como ejecutar
 # un contenedor en ECS. Se va a ejecutar con Fargate con un modo de red
-# 'awsvpc', que suele usarse con Fargate
+# 'awsvpc', que es necesario con Fargate
 resource "aws_ecs_task_definition" "web_application_task" {
   family                   = var.task_definition_name
   execution_role_arn       = var.role_iam
   task_role_arn            = var.role_iam
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
   container_definitions = jsonencode([
     {
       name      = var.container_name
