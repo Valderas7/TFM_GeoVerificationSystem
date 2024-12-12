6b37c9e0f487c39e11bca32bc448374451f378fb
Valderas7 <valderas777@gmail.com>
2024-12-13 00:40:29 +0100

    feat: añadidos elementos módulo VPC que faltaban

--- infra/modules/vpc_weather_network/main.tf
+++ infra/modules/vpc_weather_network/main.tf
@@ -1,3 +1,11 @@
+# Bloque de datos que permite el acceso a la lista de zonas de
+# disponibilidad que pueden ser accedidas por la cuenta AWS dentro de la
+# región configurada en el 'provider' de AWS
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
 # Recurso para crear una VPC con un CIDR privado en el que hay
 # un total de 256 direcciones IP
 resource "aws_vpc" "web_application_vpc" {
@@ -14,6 +22,7 @@ resource "aws_subnet" "web_application_subnets" {
   vpc_id                  = aws_vpc.web_application_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.web_application_vpc.cidr_block, 4, count.index)
   map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
   tags = {
     "Project" = "TFM"
@@ -44,5 +53,41 @@ resource "aws_security_group" "web_application_security_group" {
   tags = {
     "Project" = "TFM"
   }
+}
 
+
+# Recurso para crear una puerta de enlace de internet para que las subredes
+# públicas de la VPC definida tengan acceso a Internet, y por tanto, las
+# instancias o tareas del móulo ECS
+resource "aws_internet_gateway" "web_application_igw" {
+  vpc_id = aws_vpc.web_application_vpc.id
+
+  tags = {
+    "Project" = "TFM"
+  }
+}
+
+
+# Recurso para crear una tabla de enrutamiento para la VPC definida de forma
+# que se redirija el tráfico externo a la puerta de enlace de internet
+resource "aws_route_table" "web_application_route_table" {
+  vpc_id = aws_vpc.web_application_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.web_application_igw.id
+  }
+
+  tags = {
+    "Project" = "TFM"
+  }
+}
+
+
+# Se crean dos recursos de asociación: cada uno de ellos servirá para
+# asociar la tabla de enrutamiento con la subred correspondiente
+resource "aws_route_table_association" "web_application_route_table_association" {
+  count          = 2
+  subnet_id      = aws_subnet.web_application_subnets[count.index].id
+  route_table_id = aws_route_table.web_application_route_table.id
 }
