ae32cf0a8f91ee931b260cd670bc3004830292d3
Valderas7 <valderas777@gmail.com>
2024-12-12 01:17:48 +0100

    fix: 'outputs' del módulo 'VPC'

--- infra/modules/vpc_weather_network/outputs.tf
+++ infra/modules/vpc_weather_network/outputs.tf
@@ -1,10 +1,10 @@
 # 'Output' para almacenar las subredes del módulo
 output "subnets" {
-  value = aws_subnet.web_application_subnets
+  value = aws_subnet.web_application_subnets.id
 }
 
 
 # 'Output' para almacenar el 'Security Group' del módulo
 output "security_group" {
-  value = aws_security_group.web_application_security_group
+  value = aws_security_group.web_application_security_group.id
 }
