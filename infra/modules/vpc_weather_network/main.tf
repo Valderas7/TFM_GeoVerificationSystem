0fa9d55851cd9bc2a37c2795f651341c65f531b3
Valderas7 <valderas777@gmail.com>
2024-12-14 01:30:06 +0100

    fix: Grupo Seguridad con todos los protocolos para reglas de salida

--- infra/modules/vpc_weather_network/main.tf
+++ infra/modules/vpc_weather_network/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "web_application_security_group" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
