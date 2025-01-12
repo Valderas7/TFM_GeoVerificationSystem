74de4683ba17462a57be4dd67bb433726cce1f20
Valderas7 <valderas777@gmail.com>
2025-01-12 23:24:20 +0100

    feat: actualizada función Lambda de API Gateway recurso 'GET /clima'

--- src/lambda_query_database/lambda_function.py
+++ src/lambda_query_database/lambda_function.py
@@ -4,13 +4,17 @@ import decimal
 import json
 import urllib.parse
 import logging
+from utils.get_unique_keys_utils import (get_provinces_and_autonomous_cities,
+                                         translate_province_list)
 
-# Configuración básica del 'logging'
-logging.basicConfig(level=logging.INFO)
+# AWS ya tiene un manejador de 'logging' por defecto, por lo que se selecciona
+# y se establece su nivel a INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Se inicia cliente de recursos de DynamoDB para gestionar una tabla
 dynamo_resource = boto3.resource('dynamodb', region_name='us-east-1')
-dynamo_table = dynamo_resource.Table("Weather_DB")
+dynamo_table = dynamo_resource.Table("weather")
 
 
 # Se crea una clase para manejar los tipos de datos 'Decimal' y poder
@@ -47,27 +51,39 @@ def lambda_handler(event, context):
         # 'GET /clima/{provincia}'
         if place:
 
+            # Mensaje de 'logging' de información
+            logger.info("Se proporcionó una provincia. Recurso 'GET "
+                        "/clima/provincia'")
+
             # Se decodifica el parámetro de la provincia para convertirlo
             # a su formato original ('Cádiz', 'La Coruña', 'Málaga', etc),
-            # ya que en las URLs se transforman a palabras sin acentos ni
-            # eñes ni espacios
+            # ya que en las URLs están transformadas a sus versiones sin
+            # acentos, 'ñ' ni espacios
             place = urllib.parse.unquote(place)
 
-            # Se realiza la consulta a la tabla de DynamoDB cuya clave
-            # primaria ('Nombre') es igual a la variable 'place'
-            response = dynamo_table.get_item(Key={'Nombre': place})
-            logging.info(f"Parámetro 'provincia' recibido: {place}")
+            # Se realiza una consulta a la tabla de DynamoDB cuya clave
+            # primaria ('Nombre') es igual a la variable 'place', obteniendo
+            # todos los registros que hay con dicha clave primaria
+            response = dynamo_table.query(
+                KeyConditionExpression="Nombre = :nombre",
+                ExpressionAttributeValues={':nombre': place},
+                ScanIndexForward=False,
+                Limit=12
+            )
 
             # Después de realizar la consulta, se verifica el campo 'Item' del
-            # diccionario obtenido como respuesta a la consulta
+            # diccionario obtenido como respuesta a la consulta (debe ser una
+            # lista de diccionarios)
             item = response.get('Item', None)
 
             # Si la clave consultada no existe...
             if not item:
 
-                # Se devuelve diccionario de error
-                logging.warning(f"No se encontró la provincia '{place}' en "
-                                "la base de datos.")
+                # Mensaje de 'logging' de error
+                logger.error(f"No se encontró la provincia '{place}' en "
+                             "la base de datos.")
+
+                # Se devuelve diccionario de error con un HTTP 404
                 return {
                     "statusCode": 404,
                     "body": json.dumps({"error": "Provincia no encontrada"}),
@@ -76,7 +92,7 @@ def lambda_handler(event, context):
 
             # Se devuelve un diccionario con el código de estado HTTP 200 y
             # el JSON, sin escapar los caracteres no ASCII (tildes, etc)
-            logging.info(f"Provincia '{place}' encontrada en DynamoDB. "
+            logger.info(f"Provincia '{place}' encontrada en DynamoDB. "
                          "Retornando datos.")
             return {
                 'statusCode': 200,
@@ -87,30 +103,81 @@ def lambda_handler(event, context):
         # Si en cambio es None, el método es 'GET /clima'
         else:
 
-            # Se realiza un escaneo de la tabla de Dynamo
-            response = dynamo_table.scan()
-            logging.info("No se proporcionó una provincia. Realizando un "
-                         "escaneo de todos los registros.")
+            # Mensaje de 'logging' de información
+            logger.info("No se proporcionó provincia. Recurso 'GET /clima'")
 
-            # Después de realizar el escaneo, se verifica el campo 'Items'
-            # del diccionario obtenido como respuesta al escaneo
-            items = response.get('Items', None)
+            # Se crea una lista para ir almacenando los registros más recientes
+            # de cada clave de partición
+            recent_items_list = []
 
-            # Se devuelve un diccionario con el código de estado y el JSON
-            logging.info(f"Se encontraron {len(items)} registros en la "
-                         "base de datos.")
+            # Se llama a 'get_provinces_and_autonomous_cities' para obtener
+            # los nombres de las provincias y ciudades autónomas de España, y
+            # posteriormente 'translate_province_list' para transformar cada
+            # una de ellas a la nomenclatura que tienen en DynamoDB, teniendo
+            # así una lista con todas las claves de partición únicas
+            unique_partition_keys_list = (
+                translate_province_list(
+                    get_provinces_and_autonomous_cities()
+                )
+            )
+
+            # Para cada clave de partición...
+            for partition_key in unique_partition_keys_list:
+
+                # Se realiza una consulta a la tabla de DynamoDB indicando
+                # la clave de partición a buscar, ordenando los resultados en
+                # orden descendente según la clave de ordenación, y limitando
+                # los resultados a solo un registro (el más reciente)
+                response = dynamo_table.query(
+                    KeyConditionExpression="Nombre = :nombre",
+                    ExpressionAttributeValues={':nombre': partition_key},
+                    ScanIndexForward=False,
+                    Limit=1
+                )
+
+                # Si hay una clave 'Items' en la respuesta y la longitud de
+                # la lista de dicha clave es mayor que cero (se ha encontrado
+                # registro)...
+                if 'Items' in response and len(response['Items']) > 0:
+
+                    # Se almacena el primer (y único) elemento de la lista de
+                    # la clave 'Items', que es el registro encontrado
+                    item = response['Items'][0]
+
+                    # Mensaje de 'logging' de información
+                    logger.info(f"Encontrado registro para {item['Nombre']} "
+                                f"con tiempo Unix {item['Marca_Temporal']}")
+
+                    # Se añade el registro (un diccionario) a la lista
+                    # 'recent_items_list'
+                    recent_items_list.append(item)
+
+                # Si no...
+                else:
+
+                    # Mensaje de 'logging' de error
+                    logger.error(f"No se encontró registro para "
+                                 f"{partition_key}")
+
+            # Se devuelve un HTTP 200 con un diccionario con clave 'data' que
+            # tiene como valor la lista 'recent_items_list' que almacena todos
+            # los registros más recientes para cada provincia o ciudad
+            # autónoma (clave de partición)
             return {
                 "statusCode": 200,
-                "body": json.dumps({"data": items}, cls=DecimalEncoder)
+                "body": json.dumps({"data": recent_items_list},
+                                   cls=DecimalEncoder)
             }
 
     # Si no se puede ejecutar el bloque de código anterior se lanza una
     # excepción
     except Exception as e:
 
-        # Se devuelve un diccionario con un error HTTP 500 y el error que ha
-        # ocurrido
-        logging.error(f"Error en la función lambda_handler: {str(e)}")
+        # Mensaje de 'logging' crítico
+        logger.critical(f"Error en la función Lambda: {str(e)}")
+
+        # Se devuelve un HTTP 500 con un diccionario que muestra el error que
+        # ha ocurrido
         return {
             "statusCode": 500,
             "body": json.dumps({"error": str(e)})
---
+++ src/lambda_query_database/utils/get_unique_keys_utils.py
@@ -0,0 +1,71 @@
+# Librerías
+import pycountry
+import re
+
+
+# Función para obtener los nombres de las provincias y ciudades autónomas
+# de España
+def get_provinces_and_autonomous_cities() -> list:
+
+    # Se obtienen en una lista los datos de las provincias y ciudades
+    # autónomas de España
+    spain_provinces_and_cities_data = [
+        subdivision for subdivision in pycountry.subdivisions if
+        subdivision.country_code == "ES" and
+        (
+            subdivision.type == "Province" or
+            subdivision.type == "Autonomous city in north africa"
+        )
+    ]
+
+    # Se obtienen solamente los nombres de las provincias y ciudades autónomas
+    spain_provinces_and_cities_names = [
+        subdivision.name for subdivision in spain_provinces_and_cities_data
+    ]
+
+    # Se eliminan los paréntesis y su contenido en cada uno de los nombres,
+    # además de los "*"
+    spain_provinces_and_cities_names = [
+        re.sub(pattern=r'\[.*?\]', repl='', string=name)
+        .replace("*", "").strip()
+        for name in spain_provinces_and_cities_names
+    ]
+
+    # Se devuelve la lista con los nombres de las provincias y ciudades ya
+    # filtrados para escribirlos directamente en la API REST
+    return spain_provinces_and_cities_names
+
+
+# Diccionario de mapeo para valores de provincias
+mapeo_name = {
+    'A Coruña': 'La Coruña',
+    'Almeria': 'Almería',
+    'Araba / Álava': 'Álava',
+    'Balearic Islands': 'Islas Baleares',
+    'Biscay': 'Vizcaya',
+    'Caceres': 'Cáceres',
+    'Cadiz': 'Cádiz',
+    'Castellon': 'Castellón',
+    'Cordoba': 'Córdoba',
+    'Girona': 'Gerona',
+    'Gipuzkoa': 'Guipúzcoa',
+    'Jaen': 'Jaén',
+    'Leon': 'León',
+    'Lleida': 'Lérida',
+    'Navarre': 'Navarra',
+    'Ourense': 'Orense',
+    'Principality of Asturias': 'Asturias',
+    'Province of Huelva': 'Huelva',
+    'Seville': 'Sevilla'
+}
+
+
+# Función para transformar los valores de la lista de provincias de la
+# función 'get_provinces_and_autonomous_cities' a los valores que hay
+# almacenados en DynamoDB
+def translate_province_list(province_list: list) -> list:
+
+    # Se obtiene el valor mapeado de 'mapeo_name' para cada provincia de la
+    # lista 'province_list'. Si no existe la provincia en 'mapeo_name', se
+    # deja su valor original en la lista 
+    return [mapeo_name.get(province, province) for province in province_list]
