aa72eda900d202be58c0b05888d00cf95c82d0fc
Valderas7 <valderas777@gmail.com>
2025-01-12 01:59:39 +0100

    fix: 'range_key' (clave de ordenación) en módulo de Dynamo

--- infra/modules/dynamodb_weather_database/main.tf
+++ infra/modules/dynamodb_weather_database/main.tf
@@ -1,17 +1,19 @@
 # Se crea una tabla de DynamoDB para ir recopilando la información del clima
 # consultada en la API de OpenWeatherMap. Solo es necesario declarar la clave
-# de partición y la clave de ordenación (no todas las columnas)
+# de partición y la clave de ordenación (no todas las columnas) tanto en las
+# 'key' como en los 'attribute'
 resource "aws_dynamodb_table" "weather_db" {
   name         = var.table_name
   billing_mode = var.billing_mode
   hash_key     = var.partition_key
+  range_key    = var.sort_key
 
   attribute {
     name = var.partition_key
     type = "S"
   }
 
-    attribute {
+  attribute {
     name = var.sort_key
     type = "N"
   }
